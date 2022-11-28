52ad067f92e32f27be447a898213d65e32a0a1d6
Leonardo Campos <0000759006@senaimgaluno.com.br>
2022-11-28 18:53:37 -0300

    Adicionado a variável Nome_programador e mais uma linha de print

--- Primeiro programa.py
+++ Primeiro programa.py
@@ -1,3 +1,5 @@
 print ("Olá programador!!")
 print ("O que vamos criar hoje?")
+Nome_programador = "Leonardo Campos"
+print ("Bem vindo" , Nome_programador , ",a partir daqui iniciaremos nossa jornada!")
 
